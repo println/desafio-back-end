a79cb1ad6fcbbd13c2f79080425139654c430d34
println <feliperalph@hotmail.com>
2019-04-07 11:36:09 -0300

    Ajustes

--- src/main/java/br/com/stone/manager/employee/domains/employee/EmployeeRepository.java
+++ src/main/java/br/com/stone/manager/employee/domains/employee/EmployeeRepository.java
@@ -8,7 +8,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-@SuppressWarnings("unused")
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long>, JpaSpecificationExecutor<Employee> {
 	@Query(value = "SELECT DISTINCT role FROM Employee e WHERE LOWER(role) LIKE CONCAT('%',LOWER(:query),'%') ORDER BY role ASC")
